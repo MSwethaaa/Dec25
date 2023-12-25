ab5b6f226995ab26fb1e059ceb4c3ba49bf9f82c
DELL <Swetha>
2023-12-25 16:53:20 +0530

    Add project files.

---
+++ Dec25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec25
+{
+    internal class Program
+    {
+        static void Fun()
+        {//void method means no return keyword never return a value
+
+        }
+        public string Display()
+        {//but last statement to be return along with some type
+            return "hello";
+        }
+        public Program()//no parameter constructor
+        {
+        }
+        static void Main(string[] args)
+        {
+            Fun();//static member requires no instance of class
+            Program p = new Program();//will call line 18
+            string s = p.Display();//non static display() need to instance of class
+        }
+    }
+}
